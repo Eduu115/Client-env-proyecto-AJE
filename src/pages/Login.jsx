3a7400d2ce495be05137a793f7cb4d3e32b626ec
Edu <serranotrenado.e@gmail.com>
2025-11-12 00:24:11 +0100

    absolutely bad login disaster, MUST FIX IT

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,89 @@
 import "./Login.css";
+import { irAHome } from "./Redirects";
+import { useNavigate } from "react-router-dom"; //hook
 
 function Login() {
+  const navigate = useNavigate(); // inicio el hook
+
+  const form = document.getElementById("login-form");
+  const btn_enviar = document.querySelector(".login-button");
+  if (btn_enviar){
+
+    btn_enviar.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para manejar el inicio de sesión
+    console.log("Formulario de inicio de sesión enviado");
+    
+    // inicializamos usuario final
+    let usuarioFinal = [];
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const loginData = {
+      username: username,
+      password: password,
+    };
+
+    console.log("username:", username);
+    console.log("Password:", password);
+
+    let idPerfil = 4; // Valor predeterminado para idPerfil (Cliente)
+
+    fetch("http://localhost:9001/usuarios/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginData),  
+    
+    })
+    .then(
+      response => {
+      if (!response.ok) throw new Error(`HTTP error ${response.status}`);
+      return response.json();
+    })
+    .then((data) => {
+
+      console.log("Data recibida:", data);
+
+      usuarioFinal = data;
+
+      console.log("Success:", usuarioFinal);
+
+      alert("Inicio de sesión exitoso");
+      
+      // Guardar en el almacenamiento local
+      saveDataToLocalStorage(usuarioFinal);
+      console.log("Usuario guardado en localStorage:", usuarioFinal);
+
+
+      idPerfil = usuarioFinal.perfil.idPerfil;
+      alert("ID Perfil: " + idPerfil);
+      alert("Redirigiendo...");
+      
+      irAHome(navigate);
+
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    }),
+
+    // Reiniciar el formulario después de enviar
+    form.reset();
+    });
+  
+  }
+  
+  function saveDataToLocalStorage(x){       
+      var receivedData = JSON.stringify(x);
+      console.log(receivedData);
+      localStorage.setItem('user', receivedData);
+  }
+
+ 
+  
+
   return (
     <main className="login-page">
       <div className="login-left">
@@ -15,8 +98,8 @@ function Login() {
 
         <form className="login-form">
           <div className="login-row">
-            <label htmlFor="email">Correo electrónico</label>
-            <input id="email" name="email" type="email" />
+            <label htmlFor="username">Username</label>
+            <input id="username" name="username" type="text" />
           </div>
 
           <div className="login-row">
