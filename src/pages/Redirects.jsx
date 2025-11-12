9ae835b183511dfb4161e3796fba37ddd56b4415
Edu <serranotrenado.e@gmail.com>
2025-11-12 23:11:53 +0100

    created profile dictionary for redirects and implemented navbar at admin panel, FINISHED LOGIN

--- src/pages/Redirects.jsx
+++ src/pages/Redirects.jsx
@@ -8,16 +8,21 @@ const user = JSON.parse(localStorage.getItem("user"));
 
 let perfil = null;
 
-if (user) perfil = user.perfil;
-
+if (user) perfil = user.perfil.nombre;
+console.log("Perfil del user en Redirects.js: ", perfil);
 if (user) logged = true; 
+console.log("logged: ", logged);
+
 
 // importamos los symbol de perfiles
 import { ROLES } from "../utils/perfilSymbols.js";
 
 export function irALogin(navigate){
-    if (logged) irAHome(navigate);  
-    navigate('/login')
+    if (logged) {
+      irAHome(navigate);
+    }else {
+      navigate('/login')
+    }
 }
 
 export function irARegister(navigate){
@@ -31,26 +36,22 @@ export function irAInicio(navigate){
 // lo mandamos a su home correspondiente, los perfiles son 
 // ADMIN, CLIENTE, TRABAJADOR, JEFE que estan en perfilSymbols.js (DICCIONARIO)
 export function irAHome(navigate){
-  if (!logged){
-      irALogin(navigate);
-  }
-  else{
-    switch (perfil){
-      case ROLES.ADMIN: 
-        irAAdminInicio(navigate);
-        break;
-      case ROLES.CLIENTE:
-        navigate('/cliente/inicio');
-        break;
-      case ROLES.TRABAJADOR:
-        navigate('/trabajador/inicio');
-        break;
-      case ROLES.JEFE:
-        navigate('/jefe/inicio');
-        break;
-    default:
-      irALogin(navigate);
-    }
+  switch (perfil){
+    case ROLES.ADMIN: 
+      navigate('/admin/inicio');
+      break;
+    case ROLES.CLIENTE:
+      navigate('/cliente/inicio');
+      break;
+    case ROLES.TRABAJADOR:
+      navigate('/trabajador/inicio');
+      break;
+    case ROLES.JEFE:
+      navigate('/jefe/inicio');
+      break;
+  default:
+    irAInicio(navigate);
+    break;
   }
 }
 
--- src/utils/perfilSymbols.js
+++ src/utils/perfilSymbols.js
@@ -1,9 +1,6 @@
 export const ROLES = {
-  ADMIN: Symbol('ADMIN'),
-  CLIENTE: Symbol('CLIENTE'),
-  TRABAJADOR: Symbol('TRABAJADOR'),
-  JEFE: Symbol('JEFE')
+  ADMIN: "ROLE_ADMIN",
+  CLIENTE: "ROLE_CLIENTE",
+  TRABAJADOR: "ROLE_TRABAJADOR",
+  JEFE: "ROLE_JEFE"
 };
-
-// lo bueno de usar Symbols es que son únicos y no colisionan entre ellos
-// ni se pueden confundir con strings normales ni reescribir accidentalmente
