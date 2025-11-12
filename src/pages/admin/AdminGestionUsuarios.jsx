986c247fffd161bee4a472828940a423728f0f28
Juan Guevara <manzanillajuan41@gmail.com>
2025-11-12 17:08:20 +0100

    implement the management user

--- src/pages/admin/AdminGestionUsuarios.jsx
+++ src/pages/admin/AdminGestionUsuarios.jsx
@@ -7,7 +7,45 @@ function AdminGestionUsuarios() {
       <Navbar />
 
       <main className="admin-gestion-usuarios">
-        {/* Contenido de la vista para gestionar usuarios */}
+            <h1>Usuarios</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>NOMBRE/USER</th>
+                  <th>Correo</th>
+                  <th>ROL</th>
+                  <th>EDITAR</th>
+                  <th>VER?</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>admin1 o Carlos</td>
+                  <td>carlos.perez@example.com</td>
+                  <td>ADMIN</td>
+                  <td>icono o lo que sea</td>
+                  <td>icono o lo que sea</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>jefe1 o Laura</td>
+                  <td>laura.garcia@example.com</td>
+                  <td>JEFE</td>
+                  <td>icono o lo que sea</td>
+                  <td>icono o lo que sea</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>trabajador1 o Miguel</td>
+                  <td>miguel.rodriguez@example.com</td>
+                  <td>TRABAJADOR</td>
+                  <td>icono o lo que sea</td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
       </main>
     </>
   );
