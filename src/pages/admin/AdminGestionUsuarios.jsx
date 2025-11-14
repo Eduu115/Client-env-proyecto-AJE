c81d90ed5e19b9e629fe8175651a9097027d546c
Eduu115 <serranotrenado.e@gmail.com>
2025-11-14 17:05:39 +0100

    mapping users at user management

--- src/pages/admin/AdminGestionUsuarios.jsx
+++ src/pages/admin/AdminGestionUsuarios.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/Navbar";
 import "./AdminGestionUsuarios.css";
+import { useEffect, useState } from "react";
 
 function AdminGestionUsuarios() {
   // --------------------- LOAD USERS ---------------------
@@ -7,27 +8,32 @@ function AdminGestionUsuarios() {
   const user = JSON.parse(localStorage.getItem('user'));
 
   // cargamos los usuaruios desde una API o base de datos
-  const usuaruios = [];
+  const [usuarios, setUsuarios] = useState([]);
 
-  try {
-    const response = fetch('localhost:9001/usuarios', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Basic ${btoa(`${user.username}:${user.password}`)}` // credenciales hardcodeadas por ahora
+  useEffect(() => {
+    const cargarDatos = async () => {
+      try {
+        const response = await fetch('localhost:9001/usuarios/', { /* HAY QUE ESTAR LOGUEADO COMO A */
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Basic ${btoa(`${user.username}:${user.password}`)}` // credenciales hardcodeadas por ahora
+          }
+        });
+        // si la respuesta no es ok, lanzamos un error
+        if (!response.ok) {
+          throw new Error('Error al cargar los usuarios');
+        }
+        // cargamsoos los datos
+        const data = await response.json();
+        setUsuarios(data);
+      
+      } catch (error) {
+        console.error('Error al cargar los usuarios:', error);
       }
-    });
-    // si la respuesta no es ok, lanzamos un error
-    if (!response.ok) {
-      throw new Error('Error al cargar los usuarios');
-    }
-    // cargamsoos los datos
-    const data = response.json();
-    // Array con los user cargados
-    usuaruios = data;
-  } catch (error) {
-    console.error('Error al cargar los usuarios:', error);
-  }
+    };
+    cargarDatos();
+  }, []);
   // -------------------------------------------------------
 
   // Renderizamos la tabla de usuarios
@@ -49,30 +55,21 @@ function AdminGestionUsuarios() {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1</td>
-                <td>admin1 o Carlos</td>
-                <td>carlos.perez@example.com</td>
-                <td>ADMIN</td>
-                <td>icono o lo que sea</td>
-                <td>icono o lo que sea</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>jefe1 o Laura</td>
-                <td>laura.garcia@example.com</td>
-                <td>JEFE</td>
-                <td>icono o lo que sea</td>
-                <td>icono o lo que sea</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>trabajador1 o Miguel</td>
-                <td>miguel.rodriguez@example.com</td>
-                <td>TRABAJADOR</td>
-                <td>icono o lo que sea</td>
-                <td></td>
-              </tr>
+              {/* Ejemplo de tabla de usuarios */}
+              {usuarios.length === 0 ? (  /* Si no carga nada imprimimos esto */
+                <p>Cargando usuarios...</p>
+              ) : (
+                usuarios.map((usuario) => ( /* Si hay usuarios los mapeamos (como un foreach)*/
+                  <tr key={usuario.id}>
+                    <td>{usuario.id}</td>
+                    <td>{usuario.name} o {usuario.username}</td>
+                    <td>{usuario.email}</td>
+                    <td>{usuario.role}</td>
+                    <td>icono o lo que sea</td>
+                    <td>icono o lo que sea</td>
+                  </tr>
+                ))
+              )}
             </tbody>
           </table>
     </main>
